27077ae561e75bce1c6447953b3dc3e17706a59f
Iggi <iggirex@gmail.com>
2017-02-23 22:20:49 -0700

    all vowels in a word?

--- funcs/words.py
+++ funcs/words.py
@@ -1,23 +1,28 @@
 import scrabble
 
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-
-def isLetterDouble(letter):
-    count = 0
-    for word in scrabble.wordlist:
-        if letter + letter in word:
-            return count+1
-    return count
-
-# def checkLetter(letter):
+# alphabet = "abcdefghijklmnopqrstuvwxyz"
+# def isLetterDouble(letter):
 #     count = 0
-#     for letter in alphabet:
-#         isLetterDouble(letter)
-#         if true:
-#             count+1
+#     for word in scrabble.wordlist:
+#         if letter + letter in word:
+#             return count+1
 #     return count
+#
+# for letter in alphabet:
+#     count = isLetterDouble(letter)
+#     if count == 0:
+#         print letter
 
-for letter in alphabet:
-    count = isLetterDouble(letter)
-    if count == 0:
-        print letter
+vowels = "aeiou"
+
+def areAllVowelsInWord(word):
+    count = 0
+    for vowel in vowels:
+        if vowel in word:
+            count = count + 1
+    if count == 5:
+        return True
+
+for word in scrabble.wordlist:
+    if areAllVowelsInWord(word):
+        print (str(word) + " has all the vowels in it")
